8c76b5eaa85cc875f3007d3563a1ad636ec890a7
slymak <slymak@gmail.com>
2023-10-24 14:45:55 +0200

    once

--- once_relay_set.sh
+++ once_relay_set.sh
@@ -8,8 +8,8 @@ in3 = 16	#vou
 in4 = 21	#vin
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1, GPIO.OUT)
-#GPIO.setup(in2, GPIO.OUT)
+#GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
 #GPIO.setup(in3, GPIO.OUT)
 #GPIO.setup(in4, GPIO.OUT)
 
@@ -19,8 +19,8 @@ GPIO.setup(in1, GPIO.OUT)
 #GPIO.output(in3, GPIO.LOW)
 #GPIO.output(in4, GPIO.LOW)
 #switch OFF 
-GPIO.output(in1, GPIO.HIGH)
-#GPIO.output(in2, GPIO.HIGH)
+#GPIO.output(in1, GPIO.HIGH)
+GPIO.output(in2, GPIO.HIGH)
 #GPIO.output(in3, GPIO.HIGH)
 #GPIO.output(in4, GPIO.HIGH)
 
